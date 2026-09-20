5506584880c1cbdfe937951b0622354f88789d9c
walkercolin <walkercolin@hotmail.com>
2026-09-20 17:21:09 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haggis-golf-v2';
+const CACHE_NAME = 'haggis-golf-v3';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./app-icon.svg'];
 
 /** Stores the application shell so an already-opened app can work without reception. */
